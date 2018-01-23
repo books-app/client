7b761033b416d7d03c4cc4550ad854efd8600d1b
andriiglukhyi <andrigluxii@gmail.com>
2018-01-23 12:34:27 -0800

    bug with databse

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ $('#book-form').on('submit', function(e) {
   let data = {
     book_title: e.target.book_title.value,
     author: e.target.author.value,
-    ISBN: e.target.ISBN.value,
+    ISBN: e.target.isbn.value,
     pic_url: e.target.pic_url.value,
     descr: e.target.descr.value
   }
