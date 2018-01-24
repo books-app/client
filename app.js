841e2798a79c36ef2ced73f6bca7bcd900b328ac
andriiglukhyi <andrigluxii@gmail.com>
2018-01-24 13:07:53 -0800

    new

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function pageLoad() {
         <p>author name: ${item.author}</p>
         <p>${item.descr}</p></div>
       `;
-      $('#books').append(content);
+      $('#results').append(content);
     });
   }, function(err) {
     console.error(err);
