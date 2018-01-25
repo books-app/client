b0e192bc9ebfe476eabbe9b9651258e2a9494d3b
andriiglukhyi <andrigluxii@gmail.com>
2018-01-25 01:07:35 -0800

    new

--- scripts/views/form.js
+++ scripts/views/form.js
@@ -1,34 +1,34 @@
 
-'use strict';
+// 'use strict';
 
-(function(module) {
-  const add= {};
+// (function(module) {
+//   const add= {};
 
-  add.init = function() {
-    $('#book-form').css('display','block');
+//   add.init = function() {
+//     $('#book-form').css('display','block');
 
-    $('#book-form').on('submit', function(e) {
-      e.preventDefault();
+//     $('#book-form').on('submit', function(e) {
+//       e.preventDefault();
     
-      let data = {
-        book_title: e.target.book_title.value,
-        author: e.target.author.value,
-        ISBN: e.target.isbn.value,
-        pic_url: e.target.pic_url.value,
-        descr: e.target.descr.value
-      }
+//       let data = {
+//         book_title: e.target.book_title.value,
+//         author: e.target.author.value,
+//         ISBN: e.target.isbn.value,
+//         pic_url: e.target.pic_url.value,
+//         descr: e.target.descr.value
+//       }
     
-      $.post(`${__API_URL__}/books`, data)
-      .then(function() {
-        books.init();
-      })
-      .catch(function(err) {
-        console.error(err);
-        books.init();
-      });
-    });
+//       $.post(`${__API_URL__}/books`, data)
+//       .then(function() {
+//         books.init();
+//       })
+//       .catch(function(err) {
+//         console.error(err);
+//         books.init();
+//       });
+//     });
 
-  }
+//   }
 
-  module.add = add;
-})(window);
+//   module.add = add;
+// })(window);
