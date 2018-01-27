69d2df942350384158308e481e92332a2bdcf7de
andriiglukhyi <andrigluxii@gmail.com>
2018-01-26 18:41:33 -0800

    new

--- scripts/models/routes.js
+++ scripts/models/routes.js
@@ -1,8 +1,8 @@
+'use strict';
+
 page('/', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/new', ctx => app.bookView.initAddForm(ctx));
 page('/books/:id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
 page('/books/update', ctx => app.bookView.initUpdateForm(ctx));
 page('/books/:id/update', ctx => app.Book.fetchOne(ctx, app.bookView.initUpdateForm));
-page('/admin', () => app.adminView.initAdminPage());
-
 page();
